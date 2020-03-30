370b40fd875627b1f3ca3b502db0946759227b20
“Hassgy” <1191600519@qq.com>
2020-03-30 19:52:18 +0800

    Revert "添加按钮事件"
    This reverts commit 71b2c0fcdf5a6f88ae5f32a57f35df5d5d88d31a.
    
    # Conflicts:
    #	app/src/main/java/com/example/hassgy/ui/activity/MainActivity.java

---
+++ app/src/main/java/com/example/hassgy/domain/entity/ContentSearch.java
@@ -0,0 +1,9 @@
+package com.example.hassgy.domain.entity;
+
+import java.util.Random;
+
+public class ContentSearch {
+    public String cover = "https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/w%3D268%3Bg%3D0/sign=4bf3eac1d90735fa91f049bfa66a688e/cb8065380cd791237736dcaea0345982b3b78012.jpg";
+    public String name = "超级小白";
+    public int time = new Random().nextInt(100);
+}
