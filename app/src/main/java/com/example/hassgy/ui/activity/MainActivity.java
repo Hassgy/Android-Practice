255e17d420f8458744587261da2bc3398e03f4f8
“Hassgy” <1191600519@qq.com>
2020-03-31 14:19:04 +0800

    修改

--- app/src/main/java/com/example/hassgy/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/hassgy/ui/activity/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.Button;
 
 import com.example.hassgy.R;
 import com.example.hassgy.ui.base.BaseActivity;
-import com.example.hassgy.ui.view.MovieDetailsView;
 import com.ulfy.android.system.ActivityUtils;
 import com.ulfy.android.system.AppUtils;
 import com.ulfy.android.ui_injection.Layout;
@@ -22,16 +21,51 @@ import java.time.Instant;
 @Layout(id = R.layout.activity_main)
 public class MainActivity extends BaseActivity {
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//    @Override protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+    public static void startActivity() {
+        ActivityUtils.startActivity(MainActivity.class);
     }
 
-    @ViewClick(ids = R.id.detailPageBtn) private void gotoDetailPage(View view) {
-        Intent intent=new Intent(MainActivity.this,SettingActivity.class);
-        startActivity(intent);
-
+    /**
+     * click: startPageBtn, guidePageBtn, tagPageBtn, detailPageBtn, listPageBtn, searchPageBtn, upgradePageBtn, webViewLoadPageBtn, fileDownloadPageBtn
+     *
+     */
+    @ViewClick(ids = {R.id.startPageBtn, R.id.guidePageBtn, R.id.tagPageBtn, R.id.detailPageBtn, R.id.listPageBtn, R.id.searchPageBtn, R.id.upgradePageBtn, R.id.webViewLoadPageBtn, R.id.fileDownloadPageBtn})
+    private void clickGroup(View v) {
+        switch (v.getId()) {
+            case R.id.startPageBtn:
+                startActivity(new Intent(getContext(), SplashActivity.class));
+                break;
+            case R.id.guidePageBtn:
+//                startActivity(new Intent(getContext(), GuideActivity.class));
+                break;
+            case R.id.tagPageBtn:
+//                startActivity(new Intent(getContext(), TagPagerLinkageActivity.class));
+                break;
+            case R.id.detailPageBtn:
+                startActivity(new Intent(getContext(), SettingActivity.class));
+                break;
+            case R.id.listPageBtn:
+//                startActivity(new Intent(getContext(), .class));
+                break;
+            case R.id.searchPageBtn:
+                startActivity(new Intent(getContext(), ContentSearchActivity.class));
+                break;
+            case R.id.upgradePageBtn:
+                break;
+            case R.id.webViewLoadPageBtn:
+                break;
+            case R.id.fileDownloadPageBtn:
+                break;
+            default:
+                break;
+        }
     }
+
+
 
     @Override public void onBackPressed() {
         AppUtils.exitTwice("再按一次退出" + AppUtils.getAppName());
